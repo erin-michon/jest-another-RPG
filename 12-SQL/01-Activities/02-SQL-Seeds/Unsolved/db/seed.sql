4928097f105333dcc159fef13a12751d0a848de5
Erin M <erin.michon@gmail.com>
2022-02-26 13:44:04 -0500

    inital folder structure

---
+++ 12-SQL/01-Activities/02-SQL-Seeds/Unsolved/db/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO employees(employee_name, job_title)
+VALUES ("Max", "Instructor"), 
+("Aaron", "Teaching Assistant"), 
+("Brandon", "Teaching Assistant");
+
+INSERT INTO employee_pets(pet_name, animal_breed, emoloyee_id)
+VALUES("Donatello", "Dog","4");
+
+SELECT * FROM employee_pets
